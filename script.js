e943c36a57bcab83bd1723d6b61da82e651f81bf
KinyaDev <114854571+KinyaDev@users.noreply.github.com>
2023-05-16 15:43:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+new Typewriter('#typew', {
+  strings: ['Fluffy Idealist', 'Fluffy Otaku', "Ambitious Otaku", "Passionnated Dreamer", 'Original Worldbuilder', "Conlang Maker"],
+  autoStart: true,
+  loop: true
+});
